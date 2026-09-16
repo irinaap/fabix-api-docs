77ae66b6401e0e2816083f1e3f2b4b3d6dce015d
irinaap <irinaap@mail.ru>
2026-09-16 21:58:09 +0300

    Update config.js

--- api-docs/config.js
+++ api-docs/config.js
@@ -1,6 +1,7 @@
 // List of specifications shown in the selector (top bar).
 // url may be absolute (the server must allow CORS) or relative to this page.
 window.SWAGGER_SPECS = [
+  { name: "Fabix API", url: "../openapi.yaml" },
   { name: "gh-pages-swagger-ui-experiment", url: "https://jonasbn.github.io/gh-pages-swagger-ui-experiment/swagger.json" },
   { name: "Petstore (Swagger 2.0)", url: "https://petstore.swagger.io/v2/swagger.json" },
   { name: "Petstore (OpenAPI 3)", url: "https://petstore3.swagger.io/api/v3/openapi.json" }
